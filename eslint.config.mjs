8c93c8baa51de106a75b64853304c1be82666bfa
rasangchul <87007118+rasangchul@users.noreply.github.com>
2024-10-21 17:04:35 +0900

    coding convention
    - .gitignore에 'yarn.lock',  'package-lock.json' 을 추가했습니다.
    - prettier설정을 했습니다.
    - 들여쓰기를 4로 맞췄습니다.
    - 문자열은 싱글 쿼테이션('')으로 감싸게 맞췃습니다.
    - eslint설정을 했습니다.
    - 각 명령문 끝에 세미콜론(;)이 사용되지 않을 때 경고를 발생
    - 재할당이 발생하지 않는데 let을 사용하는 경우 에러를 발생
    - 선언했지만 사용되지 않은 변수가 있으면 에러를 발생
    - test 디렉토리 아래의 파일들에 대해서만 Jest 환경을 적용하는 ESLint 설정
    - Jest 설정에서 디버깅을 활성화하고, test 디렉토리의 .spec.js 파일을 테스트하며, node_modules는 무시하도록 정의한 설정
    - test폴더 안에 test.js, test.spec.js 를 만들어 jest를 테스트 해보았습니다.

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import globals from 'globals'; // 'globals' 패키지에서 전역 변수를 가져온다.
+import pluginJs from '@eslint/js'; // '@eslint/js' 패키지에서 JavaScript 관련 ESLint 규칙을 가져온다.
+
+export default [
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    {
+        rules: {
+            semi: 'warn', // 각 명령문 끝에 세미콜론(;)이 사용되지 않을 때 경고를 발생시킨다.
+            'prefer-const': 'error', // 재할당이 발생하지 않는데 let을 사용하는 경우 에러를 발생시킨다..
+            'no-unused-vars': 'error', // 선언했지만 사용되지 않은 변수가 있으면 에러를 발생시킨다.
+        },
+    },
+    {
+        overrides: [
+            // jest로 테스트할때 "no-undef"라고 정의되지 않은 변수를 사용하면 에러를 발생시키는데 테스트코드만 이것을 막아주는 코드이다.
+            {
+                files: ['test/**/*'],
+                env: {
+                    jest: true,
+                },
+            },
+        ],
+    },
+    {
+        extends: ['prettier'], // Prettier 규칙과 충돌하는 ESLint 규칙을 비활성화한다.
+    },
+];
