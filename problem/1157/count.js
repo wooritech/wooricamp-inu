395b897ee5ce3a3a069e00775e6afde4ede810b1
rasangchul <87007118+rasangchul@users.noreply.github.com>
2024-11-06 17:29:19 +0900

    1157번 중간 커밋

---
+++ problem/1157/count.js
@@ -0,0 +1,34 @@
+/**
+ *  알파벳 대소문자로 된 단어가 주어지면,
+ *  이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오. 단, 대문자와 소문자를 구분하지 않는다.
+ *  첫째 줄에 알파벳 대소문자로 이루어진 단어가 주어진다. 주어지는 단어의 길이는 1,000,000을 넘지 않는다.
+ *  첫째 줄에 이 단어에서 가장 많이 사용된 알파벳을 대문자로 출력한다.
+ *  단, 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력한다.
+ *
+ *  입력 예제)
+ *  Mississipi
+ *
+ *  출력 예제)
+ *  ?
+ *
+ *  입력 예제)
+ *  baaa
+ *
+ *  출력 예제)
+ *  A
+ */
+
+const input = 'baaa';
+const split = input.split('');
+const len = split.length;
+const countArr = [];
+for (let i = 0; i < len; i++) {
+    let count = 0;
+    split.forEach((e) => {
+        if (split[i] === e) {
+            count++;
+        }
+    });
+    countArr.push(count);
+}
+console.log(countArr);
